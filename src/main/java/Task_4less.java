5289ebf2b20473075b96a44780bc9760e208d3bb
George <64532466+Nu1Lifier@users.noreply.github.com>
2020-09-07 14:32:46 +0300

    add hw lesson 4

---
+++ src/main/java/Task_4less.java
@@ -0,0 +1,14 @@
+public class Task_4less {
+
+    volatile int status = 0;
+    public static void main(String[] args) {
+        Task_4less lock = new Task_4less();
+        Thread t1 = new Thread(new Task("A", lock,1));
+        Thread t2 = new Thread(new Task("B", lock,2));
+        Thread t3 = new Thread(new Task("C", lock,3));
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
